1d173c42848b6d0208bf1efb522e5c616c4b1af3
abhimanyu8829 <abhimanyucentral@gmail.com>
2026-03-10 10:39:37 +0530

    Add initial app.js file with greeting and addition functions for testing

---
+++ app.js
@@ -0,0 +1,14 @@
+// Simple JS file for ESLint and testing
+
+function greet(name) {
+  console.log(`Hello ${name}, welcome to DevOps CI/CD test!`);
+}
+
+greet("Abhimanyu");
+
+// Example function
+function add(a, b) {
+  return a + b;
+}
+
+console.log("Sum:", add(5, 3));
