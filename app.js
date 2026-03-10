ca2e3ccdade4e2a66ed10a2992537f263179920f
abhimanyu8829 <abhimanyucentral@gmail.com>
2026-03-10 10:41:01 +0530

    fix prettier formatting for app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ function greet(name) {
   console.log(`Hello ${name}, welcome to DevOps CI/CD test!`);
 }
 
-greet("Abhimanyu");
+greet('Abhimanyu');
 
 // Example function
 function add(a, b) {
   return a + b;
 }
 
-console.log("Sum:", add(5, 3));
+console.log('Sum:', add(5, 3));
